8a90078ce76f247099f6be579c00c503ed73102e
Jamie Davis <davisjam@vt.edu>
2017-11-01 13:14:00 -0400

    EHP tests for behavior in the Node VM module
    Notes:
    - REDOS can be defeated using a VM
    - IODOS cannot (won't exit until the blocking syscall returns, I guess)
    
    - VMs are good for pure JS.
      If you want more (e.g. 'require'), you have to expose more global objects.
    
    - You can achieve a more general timeout than that offered by VM by using
      the ChildProcess module instead.
    	These will be full-fledged processes with their own namespace.
    	You can manage a pool of them using the Cluster module.
    	They can be timed out and killed if they are unresponsive, e.g. a heartbeat from a setInterval.
    
    Q: If I can time out at a higher level, why do I want NodeCure?
    A: Finer-grained timeouts permit the convenience of the global namespace with the
       safety of "no EHP".

--- test/ehp/redos.js
+++ test/ehp/redos.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
+/* Node regexp match will run to completion by default.
+ * With NodeCure it will throw an exception.
+ */
 
-var len = 31; // 31: 25 seconds
+var len = 28; // 31: 25 seconds
 var str = '';
 for (var i = 0; i < len; i++)
 	str += 'a';
 str += '!'; // mismatch
 
 try {
+	console.log('Performing match with str len ' + str.length);
 	if (str.match(/(a+)+$/)) {
 		console.log('Match');
 	}
