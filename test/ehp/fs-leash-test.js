f50e9ca01bc2e22f49b602041b268636bb590813
Jamie Davis <davisjam@vt.edu>
2017-11-10 17:35:40 -0500

    Leash test: run forever

--- test/ehp/fs-leash-test.js
+++ test/ehp/fs-leash-test.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 setTimeout(() => {
 	console.log('JS: Beginning infinite loop of cheap FS reads');
 	var i = 0;
-	for (var i = 0; i < 100; i++) {
+	while (1) {
 		fs.readFileSync('/tmp/raw.dat');
 		console.log(`JS: read ${i} finished`);
 	}
