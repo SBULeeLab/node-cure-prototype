2a4f4583030b3d38f7932a2942391c4a116d5f55
ericrw96 <ericrw96@vt.edu>
2017-11-21 22:23:44 -0500

    Add in additional benchmark framework to test with a single cpu as well. Now need to add in more fs tests with writing with a variety of buffers.

--- ehp-benchmarks/run_benchmarks.sh
+++ ehp-benchmarks/run_benchmarks.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
 # setup the working directories
-git clone .. original_node
-git clone .. watchdog_node
-
-cd original_node
-git checkout "dc6bbb4"
-./configure
-make -j $(nproc)
-cd -
-
-cd watchdog_node
-git checkout NODECURE_SILENT
-./configure
-make -j $(nproc)
-cd -
+$(git clone .. original_node && cd original_node && git checkout "dc6bbb4" && ./configure && make -j $(nproc) && cd -)
+$(git clone .. watchdog_node && cd watchdog_node && git checkout NODECURE_SILENT && ./configure && make -j $(nproc) && cd -)
+$(cd benchmarks && head -c 500M /dev/urandom >random_file)
 
 echo "created node projects, now to run benchmarks"
 
@@ -26,4 +15,17 @@ do
   resultfile=results/"$(basename "${file}" .js)".result
 	echo "benchmarking ${file} see the results in ${resultfile}"
 	./bench.sh "${file}" &> "${resultfile}"
+done
+
+
+
+echo "running benchmarks witha single cpu"
+
+mkdir results_single_cpu
+shopt -s nullglob
+for file in benchmarks/*.js
+do
+  resultfile=results_single_cpu/"$(basename "${file}" .js)".result
+        echo "benchmarking ${file} with a single cpu see the results in ${resultfile}"
+        ./bench_one_cpu.sh "${file}" &> "${resultfile}"
 done
