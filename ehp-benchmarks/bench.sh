305a4e560351a469e88cc65b0da16317b29bdc16
ericrw96 <ericrw96@vt.edu>
2017-11-21 20:31:15 -0500

    Add in a flushing between each test in perf.

--- ehp-benchmarks/bench.sh
+++ ehp-benchmarks/bench.sh
@@ -2,10 +2,10 @@ file=${1}
 
 echo benchmarks for "${file}"
 echo "with the watchdog"
-time perf stat -r 50 -d watchdog_node/node "${file}"
+time perf stat -r 50 --pre "sync && sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches'" -d watchdog_node/node "${file}"
 
 echo ""
 echo ""
 echo "without the watchdog"
-time perf stat -r 50 -d original_node/node "${file}"
+time perf stat -r 50 --pre "sync && sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches'" -d original_node/node "${file}"
 
