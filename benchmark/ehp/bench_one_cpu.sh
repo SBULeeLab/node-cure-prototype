90f4fffd8959c3cc6fa982e535bb8c559e524a70
ericrw96 <ericrw96@vt.edu>
2017-11-23 09:47:28 -0500

    Refactor node executables into nodes/*/node.
    Add in instructions for running the benchmarks and setting up the nodes for benchmarking.

--- benchmark/ehp/bench_one_cpu.sh
+++ benchmark/ehp/bench_one_cpu.sh
@@ -2,10 +2,10 @@ file=${1}
 
 echo benchmarks for "${file}"
 echo "with the watchdog"
-taskset 0x1 time perf stat -r 50 --pre "sync && sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches' && sync" -d watchdog_node/node "${file}"
+taskset 0x1 time perf stat -r 50 --pre "sync && sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches' && sync" -d nodes/watchdog_node/node "${file}"
 
 echo ""
 echo ""
 echo "without the watchdog"
-taskset 0x1 time perf stat -r 50 --pre "sync && sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches' && sync" -d original_node/node "${file}"
+taskset 0x1 time perf stat -r 50 --pre "sync && sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches' && sync" -d nodes/original_node/node "${file}"
 
