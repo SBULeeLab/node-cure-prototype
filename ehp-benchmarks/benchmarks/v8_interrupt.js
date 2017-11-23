3302b0f82288d3674754e194722bbf088f13825e
ericrw96 <ericrw96@vt.edu>
2017-11-23 09:00:25 -0500

    Add in optional loop iteration parameter to the v8_interrupt test to allow the test to be tuned.
    This is done just as a single argv[2] for not have to introduce additional parsing logic nor additional node modules.

--- ehp-benchmarks/benchmarks/v8_interrupt.js
+++ ehp-benchmarks/benchmarks/v8_interrupt.js
@@ -1,10 +1,14 @@
-const N = 9999999999;
+let loop_iterations = 9999999999; // 1 billion - 1
+if (process.argv.length > 2){
+  loop_iterations = parseInt(process.argv[2],10);
+}
 
 try
 {
-	for (let i = 0; i < N; i++){
+	for (let i = 0; i < loop_iterations; i++){
   		// intentionally blank
 	}
+}
 catch(e)
 {
 }
