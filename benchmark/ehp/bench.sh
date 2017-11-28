d606d2a808ab0e5d24da8ef524a0c955f7d93da5
Jamie Davis <davisjam@vt.edu>
2017-11-28 14:08:16 -0500

    Update bench with precise vs lazy TWD runs

--- benchmark/ehp/bench.sh
+++ benchmark/ehp/bench.sh
@@ -1,11 +1,18 @@
 file=${1}
 
 echo benchmarks for "${file}"
-echo "with the watchdog"
+
+echo "vanilla"
+time perf stat -r 500 --pre "sync && sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches' && sync" -d nodes/original_node/node "${file}"
+
+echo ""
+echo ""
+echo "lazy watchdog"
+export NODECURE_TIMEOUT_WATCHDOG_TYPE=lazy
 time perf stat -r 500 --pre "sync && sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches' && sync" -d nodes/watchdog_node/node "${file}"
 
 echo ""
 echo ""
-echo "without the watchdog"
-time perf stat -r 500 --pre "sync && sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches' && sync" -d nodes/original_node/node "${file}"
-
+echo "precise watchdog"
+export NODECURE_TIMEOUT_WATCHDOG_TYPE=precise
+time perf stat -r 500 --pre "sync && sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches' && sync" -d nodes/watchdog_node/node "${file}"
