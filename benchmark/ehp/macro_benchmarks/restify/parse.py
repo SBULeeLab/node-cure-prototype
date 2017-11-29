e585d62ed6410ea7205c28d8e4646f64ceda6ee0
ericrw96 <ericrw96@vt.edu>
2017-11-29 18:02:43 -0500

    Add in restify analysis scripts.

---
+++ benchmark/ehp/macro_benchmarks/restify/parse.py
@@ -0,0 +1,15 @@
+from lib import analyze
+from functools import partial
+def json_response(line):
+	last_line = float(line.split("\"throughput\"")[1].split(",")[0].split(" ")[-1])
+	return 1/last_line * 1000
+
+def text_response(line):
+	l = float(line.split("\"throughput\"")[2].split(",")[0].split(" ")[-1])
+	return 1/l * 1000
+
+
+tests = [(json_response, 'response json'), (text_response, 'response text')]
+		
+	
+print(analyze(tests, 'restify')) 
