436cdc4c948a6f1e71fc7171135576c19d8988ef
ericrw96 <ericrw96@vt.edu>
2017-11-28 19:43:26 -0500

    Add in the koa benchmarks

---
+++ benchmark/ehp/macro_benchmarks/koa/run_one.sh
@@ -0,0 +1,9 @@
+export NODECURE_THREADPOOL_TIMEOUT_MS=9999999999
+export NODECURE_NODE_TIMEOUT_MS=999999999
+export NODECURE_SILENT=1
+export NODECURE_ASYNC_HOOKS=1
+
+cd koa/benchmarks
+export PATH="$(dirname ${1}):${PATH}"
+make
+
--- benchmark/ehp/macro_benchmarks/run_all.sh
+++ benchmark/ehp/macro_benchmarks/run_all.sh
@@ -3,7 +3,7 @@ watchdog_node=$(readlink -e "../nodes/watchdog_node/node")
 original_node=$(readlink -e "../nodes/original_node/node")
 echo "watchdog_node is ${watchdog_node}"
 echo "original_node is ${original_node}"
-for test in express
+for test in express koa ghost
 do
   echo "running benchmarks for ${test}"
   echo $(cd ${test}; ./run.sh "${watchdog_node}" "${original_node}")
