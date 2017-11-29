6ec76e80bf9b7b954006242ea9bcb771f281dddc
ericrw96 <ericrw96@vt.edu>
2017-11-28 20:50:20 -0500

    Add in final benchmarking scripts.

--- benchmark/ehp/macro_benchmarks/run_all.sh
+++ benchmark/ehp/macro_benchmarks/run_all.sh
@@ -3,7 +3,7 @@ watchdog_node=$(readlink -e "../nodes/watchdog_node/node")
 original_node=$(readlink -e "../nodes/original_node/node")
 echo "watchdog_node is ${watchdog_node}"
 echo "original_node is ${original_node}"
-for test in express koa lokijs ghost
+for test in express koa lokijs restify ws sails
 do
   echo "running benchmarks for ${test}"
   echo $(cd ${test}; ./run.sh "${watchdog_node}" "${original_node}")
