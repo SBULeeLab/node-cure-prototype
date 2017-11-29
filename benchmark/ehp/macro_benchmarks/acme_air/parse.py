90871020878353f906b443c5205d849f47ce1367
ericrw96 <ericrw96@vt.edu>
2017-11-29 17:36:57 -0500

    Add in acme air parsing.

---
+++ benchmark/ehp/macro_benchmarks/acme_air/parse.py
@@ -0,0 +1,10 @@
+from lib import analyze
+def get_average(line):
+	last_line = line.split("summary =")[-1].split("Avg:")[0].split("=")[1].split("/s")[0]
+	return (1/float(last_line)) * 1000
+
+
+tests = [(get_average, 'ACME_AIR_REQS_SECOND_AFTER_10_MINUTES')]
+		
+	
+print(analyze(tests, 'ACME_AIR')) 
