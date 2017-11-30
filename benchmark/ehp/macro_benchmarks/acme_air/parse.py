5dfc0485a8912ad7f7cfbb283f76f2e4241e2862
ericrw96 <ericrw96@vt.edu>
2017-11-30 10:05:17 -0500

    Add in the parse.pys that I forgot yesterday.

--- benchmark/ehp/macro_benchmarks/acme_air/parse.py
+++ benchmark/ehp/macro_benchmarks/acme_air/parse.py
@@ -1,7 +1,7 @@
 from lib import analyze
 def get_average(line):
 	last_line = line.split("summary =")[-1].split("Avg:")[0].split("=")[1].split("/s")[0]
-	return (1/float(last_line)) * 1000
+	return (1/float(last_line)) * 1000, last_line + "requests/second" 
 
 
 tests = [(get_average, 'ACME_AIR_REQS_SECOND_AFTER_10_MINUTES')]
