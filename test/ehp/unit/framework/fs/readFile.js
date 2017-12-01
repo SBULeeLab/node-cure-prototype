899f55ff4a1a6e991434094f6354cd3cec01c6a6
Jamie Davis <davisjam@vt.edu>
2017-12-01 00:48:59 -0500

    Test fixup

--- test/ehp/unit/framework/fs/readFile.js
+++ test/ehp/unit/framework/fs/readFile.js
@@ -1,7 +1,7 @@
 /* Needs a large-ish (100MB) file named '/tmp/raw.dat'.
  * $ dd if=/dev/zero of=/tmp/raw.dat bs=1 count=1 seek=100M */
 
-// NODECURE_THREADPOOL_TIMEOUT_MS=10 ../../../../../node readFile.js
+// NODECURE_THREADPOOL_TIMEOUT_MS=1 ../../../../../node readFile.js
 
 var fs = require('fs');
 
