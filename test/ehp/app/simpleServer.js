7ead93fe267af98cd04945d5f61286e21f36c6a5
Jamie Davis <davisjam@vt.edu>
2017-12-01 14:15:16 -0500

    simpleServer: correct throughput calculation

--- test/ehp/app/simpleServer.js
+++ test/ehp/app/simpleServer.js
@@ -13,41 +13,31 @@ if (process.argv.length > 2) {
 console.log(process.argv);
 console.log(PORT);
 
-var THROUGHPUT_REPORT_MS = 500;
+var THROUGHPUT_REPORT_MS = 100;
 var THROUGHPUT_REPORT_NS = THROUGHPUT_REPORT_MS * 1000000;
 
-var nRequests = 0;
-var timeSinceLastReport = [0,0]; // Time since last reported.
-var nReported = 0;
-function reportThroughput() {
-  var since = process.hrtime(timeSinceLastReport);
-  if (0 < since[0] || THROUGHPUT_REPORT_NS < since[1]) {
+var lifetimeRequests = 0;
+var quantumRequests = 0;
+var timeOfLastReport = [0,0]; // Time since last reported.
 
-    var instantThroughput = nRequests / (since[0] + since[1]/1000000000);
-    var sinceStartInSec = nReported * THROUGHPUT_REPORT_MS / 1000;
-    console.error(`${sinceStartInSec}: instantaneousThroughput ${instantThroughput}`);
-    //console.log(`${sinceStartInSec} ${instantThroughput}`);
-    console.log(`${instantThroughput}`);
-    nReported++;
-    nRequests = 0;
-    timeSinceLastReport = process.hrtime();
-  }
-}
+setInterval(() => {
+  if (lifetimeRequests === 0)
+    return;
+
+  var since = process.hrtime(timeOfLastReport);
+  var instantThroughput = quantumRequests / (since[0] + since[1]/1000000000); // X reqs / Y seconds
+  console.log(instantThroughput);
+  quantumRequests = 0;
+
+  timeOfLastReport = process.hrtime();
+}, 100);
 
 function recordCompletedRequest() {
-  if (nRequests === 0)
-    timeSinceLastReport = process.hrtime(); // Start now, otherwise we overestimate the start-up cost
+  if (lifetimeRequests === 0)
+    timeOfLastReport = process.hrtime(); // Start now, otherwise we overestimate the start-up cost
 
-  if (3 < process.hrtime(timeSinceLastReport)[0]) { // Long gap, reset for ease of reporting data
-    nRequests = 0;
-    timeSinceLastReport = process.hrtime();
-    nReported = 0;
-    console.log('GAP\n\n\n\n');
-    console.error('GAP\n\n\n\n');
-  }
-
-  nRequests++;
-  reportThroughput();
+  lifetimeRequests++;
+  quantumRequests++;
 }
 
 const cb = (req, resp) => {  
@@ -71,11 +61,13 @@ const handleRequest = (req, resp) => {
   if (!f.match(/(\/.+)+$/)) // ReDoS
     resp.end('Invalid file');
   else {
-    for (var i = 0; i < 1000; i++); // Do some processing.
+    var bigNum = 0;
+    for (var i = 0; i < 1000; i++) // Do some processing.
+      bigNum++;
 
     _FS.readFile(f, (err, d) => { // ReadDoS
-        //resp.end('The file you requested:' + d);
-        resp.end('read finished');
+        resp.end('The file you requested:' + d);
+        //resp.end('read finished, bigNum ' + bigNum);
         recordCompletedRequest();
     });
   }
